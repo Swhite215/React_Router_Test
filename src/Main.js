d08749a4e14467ad2d8543ef504677364858eb4b
Spencer White <spencer.white@carat.com>
2018-05-09 16:40:51 -0400

    Added test state object, and passed to component with routing.

--- src/Main.js
+++ src/Main.js
@@ -10,10 +10,24 @@ import FormTwo from "./FormTwo";
 import FormThree from "./FormThree";
 
 class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      formOneData: [
+        {name: "strategy", options: ["retail", "in-market", "awareness"]},
+        {name: "creative", options: ["250x250", "100x400", "500x650"]}
+      ],
+      formTwoData: [
+        {name: "market", options: ["gmna", "gmsa", "gme", "gmi"]},
+        {name: "duration", options: ["1 month", "3 months", "6 months", "12 months"]}
+      ]
+    }
+  }
   render () {
     return (
       <HashRouter>
       <div>
+        {/* COMMENT ME */}
         <h1>THIS IS WHERE THE CAMPAIGN NAME GOES</h1>
         <ul className="header">
           <li><NavLink exact to="/">FormOne</NavLink></li>
@@ -21,8 +35,8 @@ class Main extends Component {
           <li><NavLink to="/formThree">FormThree</NavLink></li>
         </ul>
         <div className="content">
-          <Route exact path="/" component={FormOne}/>
-          <Route path="/formTwo" component={FormTwo}/>
+          <Route exact path="/" component={() => <FormOne formData={this.state.formOneData}/>}/>
+          <Route path="/formTwo" component={() => <FormTwo formData={this.state.formTwoData}/>}/>
           <Route path="/formThree" component={FormThree}/>
         </div>
       </div>
